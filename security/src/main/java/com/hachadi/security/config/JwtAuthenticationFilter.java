949be942486e1db01f675d8b82cdc888940d64a5
HachadiIT22 <hachadi@electric-itservices.ma>
2023-03-27 03:17:38 +0000

    Completation de code d'authetification et d'autaurisaton

--- security/src/main/java/com/hachadi/security/config/JwtAuthenticationFilter.java
+++ security/src/main/java/com/hachadi/security/config/JwtAuthenticationFilter.java
@@ -6,10 +6,13 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
+import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
@@ -35,7 +38,18 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         jwt=authHeader.substring(7);
         userEmail = jwtService.extractUsername(jwt);  // Todo extract the userEmail from JWT token
         if(userEmail !=null && SecurityContextHolder.getContext().getAuthentication()==null){
-            UserDetails userDetails =this.userDetailsService.loadUserByUserName(userEmail);
+            UserDetails userDetails =this.userDetailsService.loadUserByUsername(userEmail);
+            if(jwtService.isTokenValid(jwt,userDetails)) {
+                UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities());
+                authToken.setDetails(
+                       new WebAuthenticationDetailsSource().buildDetails(request)
+                );
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+            filterChain.doFilter(request,response);
         }
     }
 }
--- security/src/main/java/com/hachadi/security/repositories/UserRepository.java
+++ security/src/main/java/com/hachadi/security/repositories/UserRepository.java
@@ -3,14 +3,13 @@ package com.hachadi.security.repositories;
 import com.hachadi.security.modals.User;
 import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
-
-import java.util.Optional;
 
 @Repository
 @Transactional
 public interface UserRepository  extends JpaRepository<User, Integer> {
 
-  Optional<User> findByEmail(String email);
+  UserDetails findByEmail(String email);
 
 }
